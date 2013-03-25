47fd2608ae2534476a96ca7288cc69859c9bd7a9
xuyannan <xyn0563@gmail.com>
2013-03-25 16:42:13 +0800

    Initial Commit

---
+++ AqiApiForIOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AqiApiForIOS
+//
+//  Created by xu yannan on 13-3-25.
+//  Copyright (c) 2013年 BLUETIGER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AqiApiForIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AqiApiForIOS
+//
+//  Created by xu yannan on 13-3-25.
+//  Copyright (c) 2013年 BLUETIGER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
